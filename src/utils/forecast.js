75fc2fc7495266fdf25e3f75471cdb8b7d4fc077
Suraj <surajmishra9113@gmail.com>
2019-08-25 12:22:22 +0530

    corrected the unit of temp. to si

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/e84c65a6714001a2112472af1b1bf401/'+latitude+','+longitude
+    const url = 'https://api.darksky.net/forecast/e84c65a6714001a2112472af1b1bf401/'+latitude+','+longitude+'?units=si'
     
     request({ url, json: true }, (error, { body }) => {
         if (error) {
